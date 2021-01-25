8059ca725476ad0d104a1263562d02093667953d
@NicChoo96 <nic_choo_96@hotmail.com>
2021-01-25 17:21:50 +0800

    Added Basic Trail making node generation
    Basic Trail Making node generation with random generated nodes and hard coded placeholder parameters

--- Project_Dementia_Game/Assets/Scripts/SpawnGrid.cs
+++ Project_Dementia_Game/Assets/Scripts/SpawnGrid.cs
@@ -6,31 +6,92 @@ using UnityEngine.UI;
 public class SpawnGrid : MonoBehaviour
 {
     public GameObject nodePrefab;
+
+    // Grid Based Nodes
     public int rowGap = 5;
     public int colGap = 5;
     public int gridSize = 3;
-    public GameObject[] nodes;
+
+    // Random Based Nodes
+    public int numberNodes = 9;
+
+
+    private GameObject[] nodes;
     private Collider2D collider2D;
 
+    // Placeholder Node generation based on grid system for trail making test
+    private void generateNodesOnGrid()
+    {
+        nodes = new GameObject[gridSize * gridSize];
+        int centerOffset = gridSize / 2;
+        int nodeCounter = 0;
+        for (int row = 0; row < gridSize; row++)
+        {
+            for (int col = 0; col < gridSize; col++)
+            {
+                nodes[nodeCounter] = Instantiate(nodePrefab, new Vector3((col - centerOffset) * colGap, (-(row - centerOffset)) * rowGap, 0), Quaternion.identity);
+                nodes[nodeCounter].name = "node x: " + (col - centerOffset) + "/y: " + (row - centerOffset);
+                nodes[nodeCounter].transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "" + (nodeCounter + 1);
+                nodeCounter++;
+            }
+        }
+    }
+
+    // Node generation checker to determine the rules to avoid overlapping circles and also trail making viable positions
+    private Vector2[] generateNodeCoordinates(int randomRange)
+    {
+        float xCoor = 0;
+        float yCoor = 0;
+        bool isExist;
+        Vector2[] nodePos = new Vector2[numberNodes];
+
+        for (int j = 0; j < numberNodes; j++) 
+        {
+
+            do
+            {
+                xCoor = Random.Range(-randomRange, randomRange);
+                yCoor = Random.Range(-randomRange, randomRange);
+                isExist = false;
+                for (int i = 0; i < numberNodes; i++)
+                {
+                    if (nodePos[i].x == xCoor && nodePos[i].y == yCoor)
+                    {
+                        isExist = true;
+                    }
+                }
+            } while (isExist);
+
+            nodePos[j].x = xCoor;
+            nodePos[j].y = yCoor;
+        }
+        
+        return nodePos;
+    }
+
+    // Generate nodes on screen
+    private void generateNodesOnRandom()
+    {
+        nodes = new GameObject[numberNodes];
+
+        int randomRange = numberNodes / 2 - 1;
+        Vector2[] nodeCoor = generateNodeCoordinates(randomRange);
+
+        int nodeCounter = 0;
+        for (int nodesCount = 0; nodesCount < numberNodes; nodesCount++)
+        {
+            nodes[nodeCounter] = Instantiate(nodePrefab, new Vector3(nodeCoor[nodesCount].x, nodeCoor[nodesCount].y, 0), Quaternion.identity);
+            nodes[nodeCounter].name = "node x: " + nodeCoor[nodesCount].x + "/y: " + nodeCoor[nodesCount].y;
+            nodes[nodeCounter].transform.GetChild(0).GetChild(0).GetComponent<Text>().text = "" + (nodeCounter + 1);
+            nodeCounter++;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         collider2D = new Collider2D();
-        nodes = new GameObject[gridSize * gridSize];
-        int centerOffset = gridSize / 2;
-        int nodeCounter = 0;
-        for (int row = 0; row < gridSize; row++) 
-        {
-            for (int col = 0; col < gridSize; col++)
-            {
-                nodes[nodeCounter] = Instantiate(nodePrefab, new Vector3((col - centerOffset) * colGap, (-(row - centerOffset)) * rowGap, 0), Quaternion.identity);
-                nodes[nodeCounter].name = "node x: " + (col- centerOffset) + "/y: " + (row - centerOffset);
-                nodes[nodeCounter].transform.GetChild(0).GetChild(0).GetComponent<Text>().text = ""+(nodeCounter+1);
-                nodeCounter++;
-            }
-        }
-            
+        generateNodesOnRandom();
     }
 
     // Update is called once per frame
