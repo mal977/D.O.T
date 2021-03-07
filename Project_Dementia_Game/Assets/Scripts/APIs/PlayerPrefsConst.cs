b66403be97b7f0d511d9b4e304c9100a4c379e36
Malcom Teh <malcomteh@gmail.com>
2021-03-08 04:22:56 +0800

    player prefs update, changed class name to playerprefsconst

---
+++ Project_Dementia_Game/Assets/Scripts/APIs/PlayerPrefsConst.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerPrefsConst
+{
+
+
+    public const string PREF_ACCESS_TOKEN = "access_token";
+    public const string PREF_NEW_TEST_ID = "new_test_id";
+   
+}
