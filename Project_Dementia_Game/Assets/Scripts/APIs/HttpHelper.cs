075b9c2e558f2aa817c16d2433790d192f863757
Malcom Teh <malcomteh@gmail.com>
2021-03-07 21:53:34 +0800

    added login method added playeprefshelper class

--- Project_Dementia_Game/Assets/Scripts/APIs/HttpHelper.cs
+++ Project_Dementia_Game/Assets/Scripts/APIs/HttpHelper.cs
@@ -9,55 +9,70 @@ using UnityEngine.Networking;
 
 public class HttpHelper : MonoBehaviour
 {
-
+   
     private string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ2NTgxMDc4LCJqdGkiOiIzM2U0ODU4ZTBiZDg0NDcxYjIzM2RmNGM1NDVmZTliMiIsInVzZXJfaWQiOjF9.g2cshvhfMZfU-w82dELwg9ypFbGc4WDSLPwXPwgnHZI";
-
+    
+    private string url = "http://172.21.148.163/";
+    private static HttpHelper instance;
+    public static HttpHelper GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new HttpHelper();
+        }
+        return instance;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        RestClient.DefaultRequestHeaders["Authorization"] = "Bearer " + token;
-        RestClient.Post<CustomResponse>("http://172.21.148.163/api/patients/new-test/", null).Then(response => {
-            EditorUtility.DisplayDialog("Json",JsonUtility.ToJson(response, true), "Ok");
-        });
+        //RestClient.DefaultRequestHeaders["Authorization"] = "Bearer " + token;
+        //RestClient.Post<CustomResponse>("http://172.21.148.163/api/patients/new-test/", null).Then(response =>
+        //{
+        //    EditorUtility.DisplayDialog("Json", JsonUtility.ToJson(response, true), "Ok");
+        //});
 
         // Handicap code to implement promises by myself @Malcom
-        testPromise().Done(() => { EditorUtility.DisplayDialog("hwe","Ewe","Ok"); }); 
+        //PostPromise().Then(() => { EditorUtility.DisplayDialog("hwe", "Ewe", "Ok"); });
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public MPromise<string> testPromise()
+    public MPromise PostPromise()
     {
-        var promise = new MPromise<string>();
-        StartCoroutine(GetText(promise));
+        var promise = new MPromise("Test");
+
         return promise;
     }
-
-    IEnumerator GetText(MPromise<string> mPromise)
+    void Awake()
     {
-        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-        UnityWebRequest www = UnityWebRequest.Post("http://172.21.148.163/api/patients/new-test/", formData);
-        www.SetRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ2NTgxMDc4LCJqdGkiOiIzM2U0ODU4ZTBiZDg0NDcxYjIzM2RmNGM1NDVmZTliMiIsInVzZXJfaWQiOjF9.g2cshvhfMZfU-w82dELwg9ypFbGc4WDSLPwXPwgnHZI");
-        yield return www.SendWebRequest();
-
-        if (www.result==UnityWebRequest.Result.ProtocolError || www.result == UnityWebRequest.Result.ConnectionError)
+        if (instance == null)
         {
-            Debug.Log(www.error);
+            instance = new HttpHelper();
         }
-        else
-        {
-            // Show results as text
-            Debug.Log(www.downloadHandler.text);
-            mPromise.Resolve();
-
-            // Or retrieve results as binary data
-            byte[] results = www.downloadHandler.data;
-        }
+        DontDestroyOnLoad(this);
     }
+
+    public void Login(String inEmail, String inPassword, Action resolveAction)
+    {
+        string path = url + "api/auth/login";
+        RestClient.Post<LoginResponse>(path, new LogInUser{ email = inEmail, password = inPassword }).Then(response =>
+        {
+            // Very hacky here, the server sends the token data as one long string, cant parse as Json cause of the single quotes
+            // So i parse it to a string, replace the single quotes with double quotes to conform to Json standards, then parse it again
+            EditorUtility.DisplayDialog("Json", JsonUtility.ToJson(response, true), "Ok");
+            PlayerPrefs.SetString(PlayerPrefsHelper.PREF_ACCESS_TOKEN, response.access_token);
+            resolveAction();
+        }).Catch(err =>{
+            EditorUtility.DisplayDialog("Error", err.ToString(), "Ok");
+            
+        });
+
+    }
+
 }
 
 [Serializable]
@@ -69,29 +84,106 @@ public class CustomResponse
     public int new_test_id;
 }
 
-// Handicap code to implement promises by myself @Malcom
-public class MPromise<T>
+[Serializable]
+public class LogInUser
 {
-    int status = 0;
-    Action onResolved;
+    public string email;
+    public string password;
 
-    public void Done(Action action)
+}
+
+[Serializable]
+public class LoginResponse
+{
+    public string email;
+    public string username;
+    public string access_token;
+    public string refresh_token;
+}
+
+// Handicap code to implement promises by myself @Malcom
+
+
+enum States
+{
+    PENDING = 0,
+    FULFILLED = 1,
+    REJECTED = 2
+
+}
+public class MPromise : MonoBehaviour
+{
+    States state;
+    string value;
+    string reason;
+
+    public Queue<MPromise> thenQueue = new Queue<MPromise>();
+    public Queue<MPromise> finallyQueue = new Queue<MPromise>();
+    public MPromise(object func)
     {
-        if (status == 0)
+        this.state = States.PENDING;
+        try
         {
-            onResolved = action;
+            if (func == typeof(System.String))
+            {
+                StartCoroutine(GetText());
+            }
+            this.onDone();
+        }
+        catch (Exception e)
+        {
+
+        }
+
+    }
+
+   
+    public MPromise Then(Type func)
+    {
+        MPromise controlledPromise = new MPromise(func);
+        this.thenQueue.Enqueue(controlledPromise);
+
+        if(this.state == States.FULFILLED)
+        {
+            this.propogateDone();
+        }
+
+        return controlledPromise;
+
+    }
+
+    private void onDone()
+    {
+        this.state = States.FULFILLED;
+        this.propogateDone();
+    }
+
+    private void propogateDone()
+    {
+        foreach (MPromise mPromise in thenQueue)
+        {
+
+        }
+    }
+
+    IEnumerator GetText()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        UnityWebRequest www = UnityWebRequest.Post("http://172.21.148.163/api/patients/new-test/", formData);
+        www.SetRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjQ2NTgxMDc4LCJqdGkiOiIzM2U0ODU4ZTBiZDg0NDcxYjIzM2RmNGM1NDVmZTliMiIsInVzZXJfaWQiOjF9.g2cshvhfMZfU-w82dELwg9ypFbGc4WDSLPwXPwgnHZI");
+        yield return www.SendWebRequest();
+
+        if (www.result == UnityWebRequest.Result.ProtocolError || www.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log(www.error);
         }
         else
         {
-            action();
-        }
-    }
-    public void Resolve()
-    {
-        status = 1;
-        if (onResolved != null)
-        {
-            onResolved();
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+            value = www.downloadHandler.text;
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
         }
     }
 }
